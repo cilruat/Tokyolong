72bb75da90150bf1ee3e1e31e756455becd4dc86
dntkdcolin <gusdl629@naver.com>
2018-06-24 16:35:39 +0900

    채팅 작업 중
    채팅 작업 중

--- SP_Client/Assets/Script/ChatElt.cs
+++ SP_Client/Assets/Script/ChatElt.cs
@@ -5,15 +5,50 @@ using UnityEngine.UI;
 
 public class ChatElt : MonoBehaviour {
 
-	// 0: you ,  1: me
-	public Text tableNo;
-	public Text[] time;
-	public Text[] chat;
-	public GameObject objYou;
-	public GameObject objMe;
+    [System.Serializable]
+    public class ChatPerson
+    {
+        public RectTransform rtPerson;
+        public RawImage imgPerson;
+        public Text textCount;
+        public Text textTableNo;
+        public Text textTime;
+        public RectTransform rtChat;
+        public Text textChatMsg;
+        public ContentSizeFitter fitter;
+    }
 
-	public void SetChat()
+    public ChatPerson[] chatPersons;
+
+    const float paddingLeft = 22f;
+    const float paddingRight = 5f;
+    const float paddingTop = 5f;
+    const float paddingBottom = 5f;
+
+//	// 0: you ,  1: me
+//	public Text tableNo;
+//	public Text[] time;
+//	public Text[] chat;
+//	public GameObject objYou;
+//	public GameObject objMe;
+
+    public void SetChat(int person, int tableNo, int personCount, int time, string msg)
 	{
-		
+        for (int i = 0; i < chatPersons.Length; i++)
+            chatPersons[i].rtPerson.gameObject.SetActive(i == person);
+
+        ChatPerson current = chatPersons[person];
+
+//        current.imgPerson.sprite = 
+
+        current.textTableNo.text = "No. <size='20'>" + tableNo.ToString() + "</size>";
+        current.textCount.text = personCount.ToString() + "명";
+        current.textTime.text = time.ToString();
+        current.textChatMsg.text = msg;
+        current.fitter.SetLayoutHorizontal();
+        current.fitter.SetLayoutVertical();
+
+
+
 	}
 }
---
+++ SP_Client/Assets/Script/ChatTableElt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatTableElt : MonoBehaviour 
+{
+    public Button btn;
+    public Image imgSelect;
+    public Text textTableNo;
+    public GameObject objNew;
+
+    public void AddNewChat()
+    {
+        if (objNew.activeSelf == false)
+            objNew.gameObject.SetActive(true);
+
+
+
+    }
+
+    public void OnSelect()
+    {
+        if (objNew.activeSelf)
+            objNew.gameObject.SetActive(false);
+    }
+}
