e81fa420465c51612f30d7947ab0f00dd2ad9e54
dntkdcolin <gusdl629@naver.com>
2018-06-30 15:56:12 +0900

    .
    .

---
+++ SP_Client/Assets/Script/Info.Class.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo
+{
+    public int tableNo = -1;
+    public byte peopleCnt = 0;
+    public byte customerType = 0;
+}
+
+
+public partial class Info : MonoBehaviour 
+{
+    List<UserInfo> listUser = new List<UserInfo>();
+}
