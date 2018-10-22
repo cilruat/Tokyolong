0627a29173252fd957e574e10a3e75e74946cb05
cilruat <37917548+cilruat@users.noreply.github.com>
2018-10-23 01:15:36 +0900

    ..
    ..

--- SP_Client/Assets/TwoCars/Assets/Scripts/Managers/AudioManager.cs
+++ SP_Client/Assets/TwoCars/Assets/Scripts/Managers/AudioManager.cs
@@ -12,97 +12,99 @@
 using UnityEngine;
 using System.Collections;
 
-public class AudioManager : MonoBehaviour {
+namespace TwoCars {
+	public class AudioManager : MonoBehaviour {
 
-    #region Game Spesific
+	    #region Game Spesific
 
-    #endregion
+	    #endregion
 
-    #region Template Fields
-    public AudioSource musicSource;
-	public AudioSource soundSource;
-        
-	public AudioClip gameMusic;
-	public AudioClip uiClick;
-    public AudioClip collect;
-    public AudioClip winSound;
-    public AudioClip loseSound;
-    public AudioClip popUpOpen;
-    public AudioClip popUpClose;
-    #endregion
+	    #region Template Fields
+	    public AudioSource musicSource;
+		public AudioSource soundSource;
+	        
+		public AudioClip gameMusic;
+		public AudioClip uiClick;
+	    public AudioClip collect;
+	    public AudioClip winSound;
+	    public AudioClip loseSound;
+	    public AudioClip popUpOpen;
+	    public AudioClip popUpClose;
+	    #endregion
 
-    #region Sound FX Methods
-    public void PlayLoseSound()
-	{
-		StopGameMusic ();
-		soundSource.clip = loseSound;
-		soundSource.Play ();
+	    #region Sound FX Methods
+	    public void PlayLoseSound()
+		{
+			StopGameMusic ();
+			soundSource.clip = loseSound;
+			soundSource.Play ();
+		}
+
+		public void PlayUIClick()
+		{
+			soundSource.clip = uiClick;
+			soundSource.Play ();
+		}
+
+		public void PlayWinSound()
+		{
+			StopGameMusic ();
+			soundSource.clip = winSound;
+			soundSource.Play ();
+		}
+
+	    public void PlayCollectSound()
+	    {
+	        soundSource.clip = collect;
+	        soundSource.Play();
+	    }
+	    public void PlaySplashScreenSound()
+	    {
+
+	    }
+
+	    public void PlayPopUpOpenSound()
+	    {
+
+	    }
+
+	    public void PlayPopUpCloseSound()
+	    {
+
+	    }
+
+
+	    public void SetSoundFxVolume(float value)
+		{
+			float temp = value + soundSource.volume;
+			if (temp < 0 || temp > 1)
+				return;
+			else
+				soundSource.volume += value;
+		}
+		#endregion
+
+		#region Music Methods
+		public void PlayGameMusic()
+		{
+			musicSource.clip = gameMusic;
+			musicSource.Play ();
+		}
+
+		public void StopGameMusic()
+		{
+			musicSource.Stop ();
+		}
+
+		public void SetSoundMusicVolume(float value)
+		{
+			float temp = value + musicSource.volume;
+			if (temp < 0 || temp > 1)
+				return;
+			else
+				musicSource.volume += value;
+		}
+		#endregion
+
 	}
-
-	public void PlayUIClick()
-	{
-		soundSource.clip = uiClick;
-		soundSource.Play ();
-	}
-
-	public void PlayWinSound()
-	{
-		StopGameMusic ();
-		soundSource.clip = winSound;
-		soundSource.Play ();
-	}
-
-    public void PlayCollectSound()
-    {
-        soundSource.clip = collect;
-        soundSource.Play();
-    }
-    public void PlaySplashScreenSound()
-    {
-
-    }
-
-    public void PlayPopUpOpenSound()
-    {
-
-    }
-
-    public void PlayPopUpCloseSound()
-    {
-
-    }
-
-
-    public void SetSoundFxVolume(float value)
-	{
-		float temp = value + soundSource.volume;
-		if (temp < 0 || temp > 1)
-			return;
-		else
-			soundSource.volume += value;
-	}
-	#endregion
-
-	#region Music Methods
-	public void PlayGameMusic()
-	{
-		musicSource.clip = gameMusic;
-		musicSource.Play ();
-	}
-
-	public void StopGameMusic()
-	{
-		musicSource.Stop ();
-	}
-
-	public void SetSoundMusicVolume(float value)
-	{
-		float temp = value + musicSource.volume;
-		if (temp < 0 || temp > 1)
-			return;
-		else
-			musicSource.volume += value;
-	}
-	#endregion
-
 }
