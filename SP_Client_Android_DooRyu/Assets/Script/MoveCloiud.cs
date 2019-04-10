a6e1b530ef25e152451075264be0a8a07915c919
cuyas <cuyas@naver.com>
2019-04-10 15:06:24 +0900

    로그인화면 수정 1
    벚꽃 구름

---
+++ SP_Client_Android_DooRyu/Assets/Script/MoveCloiud.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCloiud : MonoBehaviour {
+	private float TimeLeft = 10.0f;
+	private float nextTime = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (new Vector3 (-1, 0, 0) * Time.deltaTime);
+	}
+}
