5ddab72f280e6ebe4e5ea9403a62afbb7e7039f2
cuyas <cuyas@naver.com>
2019-04-23 18:09:22 +0900

    추가게임2 Race : 지그재그런
    2

---
+++ SP_Client_Android_DooRyu/Assets/Game/Racing/ZIRAssets/CS_Assets/CS_Scripts/ZIRLockRotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Zigzag
+{
+	/// <summary>
+	/// Locks the current rotation of the object. Used to keep the camera looking in the same direction
+	/// </summary>
+	public class ZIRLockRotation : MonoBehaviour
+	{
+		internal Transform thisTransform;
+		internal Quaternion initialRotation;
+		
+		void Start()
+		{
+			thisTransform = transform;
+			
+			// Set the initial rotation of the object
+			initialRotation = thisTransform.rotation;
+		}
+		
+		void LateUpdate()
+		{
+			// Keep the rotation of the object to the initial rotation
+			thisTransform.rotation = initialRotation;
+		}
+	}
+}
