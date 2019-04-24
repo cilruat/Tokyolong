4fbb5617bffdd742830756b50a03f6d433b323b8
cuyas <cuyas@naver.com>
2019-04-24 13:40:01 +0900

    추가게임7 Arcade : 두더지잡기
    7

---
+++ SP_Client_Android_DooRyu/Assets/Game/Arcade/WAMAssets/CS_Assets/CS_Scripts/Types/Level.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+
+namespace WhackAMole.Types
+{
+	/// <summary>
+	/// This script defines a level in the game. When the player reaches a certain score, the level is increased and the difficulty is changed accordingly
+	/// This class is used in the Game Controller script
+	/// </summary>
+	[Serializable]
+	public class Level
+	{
+        [Tooltip("The score needed to win this level")]
+        public int scoreToNextLevel = 200;
+
+        [Tooltip("The time bonus for finishing this level")]
+        public float timeBonus = 5;
+
+        [Tooltip("The maximum number of targets allowed on screen at once")]
+        public int maximumTargets = 2;
+	}
+}
