7f901b5526b7946609fc35975c2fff38e3c57095
cuyas <cuyas@naver.com>
2019-04-25 22:57:14 +0900

    추가게임13 Race : 좀비레이싱
    13

---
+++ SP_Client_Android_DooRyu/Assets/Game/Racing/ZDGAssets/CS_Assets/CS_Scripts/ZDGGibExplode.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace ZombieDriveGame
+{
+    /// <summary>
+    /// This script defines an explosion of gibs. Each gib must have a rigidbody attached to it.
+    /// </summary>
+    public class ZDGGibExplode : MonoBehaviour
+    {
+        [Tooltip("A list of the objects that will be exploded")]
+        public Rigidbody[] gibs;
+
+        [Tooltip("The explosion power. The objects fly randomly")]
+        public float explodePower = 100;
+
+        internal int index;
+        
+        public void Start()
+        {
+            Explode();
+        }
+
+        public void Explode()
+        {
+            // Go through all the gibs, and throw them in a random direction
+            for ( index = 0; index < gibs.Length; index++ )
+            {
+                // Throw the object in a random direction
+                gibs[index].AddForce(new Vector3(Random.Range(-explodePower, explodePower), Random.Range(explodePower * 2, explodePower * 3), Random.Range(-explodePower, explodePower)));
+
+                // Give the object a random rotation force
+                gibs[index].AddTorque(new Vector3(Random.Range(-explodePower, explodePower), Random.Range(-explodePower, explodePower), Random.Range(-explodePower, explodePower)));
+            }
+        }
+    }
+}
---
+++ SP_Client_Android_DooRyu/Assets/Game/Racing/ZDGAssets/CS_Assets/CS_Scripts/ZDGTouchable.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace ZombieDriveGame
+{
+    /// <summary>
+    /// This script defines an object which can interact with the player in various ways. A touchable may be a rock or a railing that
+    /// bounces the player back, or it can be a zombie that the player can run over, or it can be an item that can be collected.
+    /// </summary>
+    public class ZDGTouchable : MonoBehaviour
+    {
+        [Tooltip("The tag of the object that can touch this block")]
+        public string touchTargetTag = "Player";
+
+        [Tooltip("The function that runs when this object is touched by the target")]
+        public string touchFunction = "ChangeScore";
+
+        [Tooltip("The parameter that will be passed with the function")]
+        public float functionParameter = 100;
+
+        [Tooltip("The target object that the function will play from")]
+        public string functionTarget = "GameController";
+
+        [Tooltip("The effect that is created at the location of this object when it is touched")]
+        public Transform touchEffect;
+
+        [Tooltip("A random rotation given to the object only on the Y axis")]
+        public Vector2 rotationRange = new Vector2(0,360);
+        
+        void Start()
+        {
+            transform.eulerAngles = Vector3.up * Random.Range( rotationRange.x, rotationRange.y);
+        }
+
+        /// <summary>
+        /// Is executed when this obstacle touches another object with a trigger collider
+        /// </summary>
+        /// <param name="other"><see cref="Collider"/></param>
+        void OnTriggerEnter(Collider other)
+        {
+            // Check if the object that was touched has the correct tag
+            if (other.tag == touchTargetTag)
+            {
+                // Check that we have a target tag and function name before running
+                if (touchFunction != string.Empty)
+                {
+                    // Run the function
+                    GameObject.FindGameObjectWithTag(functionTarget).SendMessage(touchFunction, functionParameter);
+                }
+
+                // If there is a touch effect, create it
+                if (touchEffect) Instantiate(touchEffect, transform.position, transform.rotation);
+                
+                // Remove the object from the game
+                Destroy(gameObject);
+            }
+        }
+    }
+}
