9b065de54527ee25456df4d0750412896c9ec73c
cuyas <cuyas@naver.com>
2019-12-08 20:30:46 +0900

    1208 메뉴, 홈화면 누적
    .

--- SP_Client_Android/Assets/Script/MoveCloud.cs
+++ SP_Client_Android/Assets/Script/MoveCloud.cs
@@ -8,27 +8,31 @@ public class MoveCloud : MonoBehaviour {
 	void Start()//스타트 코루틴
 	{
 		StartCoroutine(Move());
+
 	}
 
 	IEnumerator Move()
 	{
-		rtLogo.anchoredPosition = new Vector2(700f, 30f);//rt 로고를 정의해주기, 시작하는 위치설정 
+		rtLogo.anchoredPosition = new Vector2(250f, 75f);//rt 로고를 정의해주기, 시작하는 위치설정 
 
 		while (true)
 		{
 			float posX = rtLogo.anchoredPosition.x;//변수들을 정의
-			float elapsed = Time.deltaTime *10f;//초당움직이는 프레임, 프레임은 유니티내부에서 설정 가능
-			float moveX = Mathf.MoveTowards(posX, -700f, elapsed);//목표방향,계속 쏴주기
+			float elapsed = Time.deltaTime *25f;//초당움직이는 프레임, 프레임은 유니티내부에서 설정 가능
+			float moveX = Mathf.MoveTowards(posX, -100f, elapsed);//목표방향,계속 쏴주기
 
-			rtLogo.anchoredPosition = new Vector2(moveX, 30f);
+			rtLogo.anchoredPosition = new Vector2(moveX, 75f);
 
-			if (rtLogo.anchoredPosition.x <= -700f)//목표지점까지가면끝입니다
+			if (rtLogo.anchoredPosition.x <= -100f)//목표지점까지가면끝입니다
 				break;
 
 			yield return null;
 		}
 
+
 		Debug.Log("End Move");
 		StartCoroutine(Move());
 	}
+		
+
 }
