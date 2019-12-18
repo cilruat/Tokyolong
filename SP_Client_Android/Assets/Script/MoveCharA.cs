a6b112fcaa62b93a4b4ba6dafa820711748bd705
cuyas <cuyas@naver.com>
2019-12-18 17:14:39 +0900

    MerryChristMas_업데이트
    .

--- SP_Client_Android/Assets/Script/MoveCharA.cs
+++ SP_Client_Android/Assets/Script/MoveCharA.cs
@@ -12,17 +12,17 @@ public class MoveCharA : MonoBehaviour {
 
 	IEnumerator Move()
 	{
-		character.anchoredPosition = new Vector2(0f, 30f);//rt 로고를 정의해주기, 시작하는 위치설정 
+		character.anchoredPosition = new Vector2(30f, -15f);//rt 로고를 정의해주기, 시작하는 위치설정 
 
 		while (true)
 		{
 			float posX = character.anchoredPosition.x;//변수들을 정의
-			float elapsed = Time.deltaTime *10f;//초당움직이는 프레임, 프레임은 유니티내부에서 설정 가능
-			float moveX = Mathf.MoveTowards(posX, 150f, elapsed);//목표방향,계속 쏴주기
+			float elapsed = Time.deltaTime *3f;//초당움직이는 프레임, 프레임은 유니티내부에서 설정 가능
+			float moveX = Mathf.MoveTowards(posX, 80f, elapsed);//목표방향,계속 쏴주기
 
-			character.anchoredPosition = new Vector2(moveX, 30f);
+			character.anchoredPosition = new Vector2(moveX, -15f);
 
-			if (character.anchoredPosition.x >= 150f)//목표지점까지가면끝입니다
+			if (character.anchoredPosition.x >= 80f)//목표지점까지가면끝입니다
 				break;
 
 			yield return null;
