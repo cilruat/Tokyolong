27da77f4c9df4c5d461dd18eb8ca5cc65bbd3a90
cuyas <cuyas@naver.com>
2020-03-08 14:11:34 +0900

    1.군대썰완성
    .

---
+++ SP_Client_Android/Assets/Script/Contents/Ssul/TabButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabButton : MonoBehaviour {
+
+	Image background;
+	public Sprite idleImg;
+	public Sprite selectImg;
+
+	private void Awake()
+	{
+		background = GetComponent<Image> ();
+	}
+
+	public void Selected()
+	{
+		background.sprite = selectImg;
+	}
+
+	public void DeSelected()
+	{
+		background.sprite = idleImg;
+	}
+}
--- SP_Client_Android/Assets/Script/Contents/Ssul/TabPanel.cs
+++ SP_Client_Android/Assets/Script/Contents/Ssul/TabPanel.cs
@@ -10,16 +10,26 @@ public class TabPanel : MonoBehaviour {
 	public List<TabButton> tabButtons;
 	public List<GameObject> contentsPanels;
 
+	int selected = 0;
+
+	private void Start()
+	{
+		ClickTab (selected);
+	}
+		
 	public void ClickTab(int id)
 	{
 		for (int i = 0; i < contentsPanels.Count; i++) 
 		{
 			if (i == id) {
 				contentsPanels [i].SetActive (true);
+				tabButtons [i].Selected ();
 			} 
 			else 
 			{
 				contentsPanels [i].SetActive (false);
+				tabButtons [i].DeSelected ();
+
 			}
 		}
 
