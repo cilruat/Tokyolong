b74b342b3867d0b0b0bd35b77e8610813c4737ce
cuyas <cuyas@naver.com>
2020-05-31 10:45:15 +0900

    인싸 3번째 컨텐츠중.

---
+++ SP_Client_Android/Assets/Game/0SIDEGAME/Inssa/InSsaGoHome.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InSsaGoHome : MonoBehaviour {
+
+	public void GoHome()
+	{
+
+		SceneManager.LoadScene ("InSsa");
+	}
+}
--- SP_Client_Android/Assets/Script/Contents/Inssa/InssaRandomGame.cs
+++ SP_Client_Android/Assets/Script/Contents/Inssa/InssaRandomGame.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using UnityEngine.SceneManagement;
 
 public class InssaRandomGame : MonoBehaviour {
 
@@ -11,8 +11,13 @@ public class InssaRandomGame : MonoBehaviour {
 	System.Random InssaRandomizer = new System.Random(); //여기 클래스에서 랜덤 선언?
 	Transform RandomPanel;
 
-	public GameObject animPanel;
 
+	public Animator animTreasure;
+	public bool TreasurePlayAnim = false;
+
+
+	public GameObject animPanel;
+	public GameObject TreasureanimPanel;
 
 
 	public void OnClickRemoteControl()
@@ -24,6 +29,15 @@ public class InssaRandomGame : MonoBehaviour {
 			anim.Play ("TVAnim");
 			PlayAnim = true;
 
+		}
+	}
+
+	public void OnClickTreaure()
+	{
+		if (TreasurePlayAnim == false) {
+			TreasureanimPanel.SetActive (true);
+			animTreasure.Play ("InssaEffect");
+			TreasurePlayAnim = true;
 		}
 	}
 
@@ -90,9 +104,14 @@ public class InssaRandomGame : MonoBehaviour {
 
 		}
 
+	}
 
-
-
+	public void OnTreasureAnimComplete()
+	{
+		SceneManager.LoadScene ("InssaGatcha");
+		//TreasurePlayAnim = false;
+		//TreasureanimPanel.SetActive (false);
+		Debug.Log ("GOGOGO?");
 	}
 
 	IEnumerator RestartAnim(){
@@ -102,56 +121,4 @@ public class InssaRandomGame : MonoBehaviour {
 	}
 
 
-
-/*public void Randomize()
-	{
-		
-		RandomPanel = GameObject.Find ("Canvas").gameObject.transform.GetChild (0).gameObject.transform;
-
-
-			for (int i = 0; i < RandomPanel.childCount; i++) {
-				RandomPanel.GetChild (i).gameObject.SetActive (false);
-			}
-
-			switch (InssaRandomizer.Next (0, GameObject.Find ("Canvas").gameObject.transform.GetChild (0).gameObject.GetComponent<RandomGameGetList> ().ImageList.Count)) {
-			case 0:
-				RandomPanel.GetChild (0).gameObject.SetActive (true);
-				break;
-
-			case 1:
-				RandomPanel.GetChild (1).gameObject.SetActive (true);
-				break;
-
-			case 2:
-				RandomPanel.GetChild (2).gameObject.SetActive (true);
-				break;
-
-			case 3:
-				RandomPanel.GetChild (3).gameObject.SetActive (true);
-				break;
-
-			case 4:
-				RandomPanel.GetChild (4).gameObject.SetActive (true);
-				break;
-
-			case 5:
-				RandomPanel.GetChild (5).gameObject.SetActive (true);
-				break;
-
-			case 6:
-				RandomPanel.GetChild (6).gameObject.SetActive (true);
-				break;
-
-			case 7:
-
-				RandomPanel.GetChild (7).gameObject.SetActive (true);
-				break;
-
-			default:
-				break;
-
-			}
-
-		}*/
-
-	}
+}
