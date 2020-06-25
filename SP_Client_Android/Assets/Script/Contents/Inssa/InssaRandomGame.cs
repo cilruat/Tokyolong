78e78ff3c38ff64b8983c4360fdb00279ccd0faf
cuyas <cuyas@naver.com>
2020-06-25 15:58:13 +0900

    .
    .

--- SP_Client_Android/Assets/Script/Contents/Inssa/InssaRandomGame.cs
+++ SP_Client_Android/Assets/Script/Contents/Inssa/InssaRandomGame.cs
@@ -18,7 +18,7 @@ public class InssaRandomGame : MonoBehaviour {
 
 	public GameObject animPanel;
 	public GameObject TreasureanimPanel;
-
+	public GameObject Alarm;
 
 	public void OnClickRemoteControl()
 	{
@@ -99,7 +99,174 @@ public class InssaRandomGame : MonoBehaviour {
 			RandomPanel.GetChild (7).gameObject.SetActive (true);
 			break;
 
-		default:
+
+			case 8:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(8).gameObject.SetActive(true);
+				break;
+			case 9:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(9).gameObject.SetActive(true);
+				break;
+			case 10:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(10).gameObject.SetActive(true);
+				break;
+			case 11:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(11).gameObject.SetActive(true);
+				break;
+			case 12:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(12).gameObject.SetActive(true);
+				break;
+			case 13:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(13).gameObject.SetActive(true);
+				break;
+			case 14:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(14).gameObject.SetActive(true);
+				break;
+			case 15:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(15).gameObject.SetActive(true);
+				break;
+			case 16:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(16).gameObject.SetActive(true);
+				break;
+			case 17:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(17).gameObject.SetActive(true);
+				break;
+			case 18:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(18).gameObject.SetActive(true);
+				break;
+			case 19:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(19).gameObject.SetActive(true);
+				break;
+			case 20:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(20).gameObject.SetActive(true);
+				break;
+			case 21:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(21).gameObject.SetActive(true);
+				break;
+			case 22:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(22).gameObject.SetActive(true);
+				break;
+			case 23:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(23).gameObject.SetActive(true);
+				break;
+			case 24:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(24).gameObject.SetActive(true);
+				break;
+			case 25:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(25).gameObject.SetActive(true);
+				break;
+			case 26:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(26).gameObject.SetActive(true);
+				break;
+			case 27:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(27).gameObject.SetActive(true);
+				break;
+			case 28:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(28).gameObject.SetActive(true);
+				break;
+			case 29:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(29).gameObject.SetActive(true);
+				break;
+			case 30:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(30).gameObject.SetActive(true);
+				break;
+			case 31:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(31).gameObject.SetActive(true);
+				break;
+			case 32:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(32).gameObject.SetActive(true);
+				break;
+			case 33:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(33).gameObject.SetActive(true);
+				break;
+			case 34:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(34).gameObject.SetActive(true);
+				break;
+			case 35:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(35).gameObject.SetActive(true);
+				break;
+			case 36:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(36).gameObject.SetActive(true);
+				break;
+			case 37:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(37).gameObject.SetActive(true);
+				break;
+			case 38:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(38).gameObject.SetActive(true);
+				break;
+			case 39:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(39).gameObject.SetActive(true);
+				break;
+			case 40:
+				StartCoroutine(RestartAnim());
+
+				RandomPanel.GetChild(40).gameObject.SetActive(true);
+				break;
+
+			default:
 			break;
 
 		}
@@ -111,7 +278,6 @@ public class InssaRandomGame : MonoBehaviour {
 		SceneManager.LoadScene ("InssaGatcha");
 		//TreasurePlayAnim = false;
 		//TreasureanimPanel.SetActive (false);
-		Debug.Log ("GOGOGO?");
 	}
 
 	IEnumerator RestartAnim(){
