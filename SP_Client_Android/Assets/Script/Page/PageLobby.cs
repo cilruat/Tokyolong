f650c5a59caa8039071f35752da34067b925954d
cuyas <cuyas@naver.com>
2020-11-24 17:26:01 +0900

    .
    .

---
+++ SP_Client_Android/Assets/Script/Page/PageLobby.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PageLobby : PageBase
+{
+
+    public GameObject objBoard;
+
+
+    public Text txtPlayCnt;
+    public Text txtTableNo;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        txtPlayCnt.text = Info.GamePlayCnt.ToString();
+        txtTableNo.text = Info.TableNum.ToString();
+        //Info.practiceGame = false;
+    }
+
+    public void RefreshGamePlayChance()
+    {
+        if (Info.isCheckScene("Lobby") == false)
+            return;
+
+        txtPlayCnt.text = Info.GamePlayCnt.ToString();
+
+    }
+
+
+    public void ReturnHome()
+    {
+        SceneChanger.LoadScene("Main", objBoard);
+    }
+
+    public void ReturnFirst()
+    {
+        SceneChanger.LoadScene("SelectGame", objBoard);
+    }
+
+
+
+
+}
---
+++ SP_Client_Android/Assets/Script/Page/PageSelectGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PageSelectGame : PageBase  {
+
+
+    public GameObject objBoard;
+    public Text txtPlayCnt;
+    public Text txtTableNo;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        txtPlayCnt.text = Info.GamePlayCnt.ToString();
+        txtTableNo.text = Info.TableNum.ToString();
+        //Info.practiceGame = false;
+    }
+
+    public void RefreshGamePlayChance()
+    {
+        if (Info.isCheckScene("SelectGame") == false)
+            return;
+
+        txtPlayCnt.text = Info.GamePlayCnt.ToString();
+
+    }
+
+
+    public void OnGoSingle()
+    {
+        SceneChanger.LoadScene("Game", objBoard);
+    }
+
+    public void OnGoLobby()
+    {
+        SceneChanger.LoadScene("Lobby", objBoard);
+    }
+
+    public void ReturnHome()
+    {
+        SceneChanger.LoadScene("Main", objBoard);
+    }
+
+
+}
