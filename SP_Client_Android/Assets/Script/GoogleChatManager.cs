95991472f26d0f4b800d0f03da8d4ac4f0224cf9
cuyas <cuyas@naver.com>
2020-11-27 17:35:03 +0900

    .
    .

--- SP_Client_Android/Assets/Script/GoogleChatManager.cs
+++ SP_Client_Android/Assets/Script/GoogleChatManager.cs
@@ -8,17 +8,29 @@ using UnityEngine.Networking;
 public class GoogleChatManager : MonoBehaviour {
 
 
+    // 영남대
+    //https://docs.google.com/spreadsheets/d/1osVtywsdEM9J4-w8Dhs_Gm0bBhYB1fCAR40LK23WlGg/export?format=tsv&range=B:B
+    //https://script.google.com/macros/s/AKfycbxIf7mzSmD-B0QXe-XlA-Tt9VkiuflhEElj1p3EKgeFK32aoUN-/exec
+
+
     //각각 매장마다 단순 URL 주소를 입력 변경
-    const string URL = "";
-    const string WebURL = "";
+    const string URL = "https://docs.google.com/spreadsheets/d/1osVtywsdEM9J4-w8Dhs_Gm0bBhYB1fCAR40LK23WlGg/export?format=tsv&range=B:B";
+
+    //구글 스프레드시트 코드 - 게시 -웹배포 URL 
+    const string WebURL = "https://script.google.com/macros/s/AKfycbxIf7mzSmD-B0QXe-XlA-Tt9VkiuflhEElj1p3EKgeFK32aoUN-/exec";
+
 
     public Text ChatText;
-    public InputField NicknameInput, Chatinput;
+    public InputField NicknameInput, ChatInput;
 
 
-    private void Start()
+
+
+    void Start()
     {
+
         StartCoroutine(Get());
+
     }
 
 
@@ -31,21 +43,23 @@ public class GoogleChatManager : MonoBehaviour {
         ChatText.text = data;
 
         StartCoroutine(Get());
-
     }
+
+
 
     public void ChatPost()
     {
         WWWForm form = new WWWForm();
         form.AddField("nickname", NicknameInput.text);
-        form.AddField("chat", Chatinput.text);
+        form.AddField("chat", ChatInput.text);
 
         StartCoroutine(Post(form));
     }
 
+
     IEnumerator Post(WWWForm form)
     {
-        using(UnityWebRequest www = UnityWebRequest.Post(WebURL, form)) //반드시 using을 써야한다
+        using (UnityWebRequest www = UnityWebRequest.Post(WebURL, form)) // 반드시 using을 써야한다
         {
             yield return www.SendWebRequest();
         }
