f16fb9e1a9e932ae4d0823a575f926511ccdc852
cuyas <42237920+cuyas@users.noreply.github.com>
2020-12-08 05:33:08 +0900

    .
    .

--- SP_Client_Android/Assets/Game/0SIDEGAME/Inssa/Scripts/Gatcha.cs
+++ SP_Client_Android/Assets/Game/0SIDEGAME/Inssa/Scripts/Gatcha.cs
@@ -2,23 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class Gatcha : MonoBehaviour {
 
+public GameObject objBoard;
+public Text Text;
+public GameObject objReSetBtn;
+public GameObject FingerClap;
 
 
-    public Text Text;
+
+
     public List<string> GachaList = new List<string>();
+
+
+    private void Start()
+    {
+        objReSetBtn.SetActive(false);
+        FingerClap.SetActive(true);
+
+    }
+
 
     public void Gacha()
     {
         for (int i = 0; i < 1; i++)
         {
+            if(GachaList.Count != 0)
+            {
             int rand = Random.Range(0, GachaList.Count);
             print(GachaList[rand]);
             Text.text = GachaList[rand].ToString();
             GachaList.RemoveAt(rand);
+            }
+            else
+            {
+                objReSetBtn.SetActive(true);
+                FingerClap.SetActive(false);
+            }
         }
     }
+
+    public void Reset()
+    {
+        SceneChanger.LoadScene("InssaFingerClap", objBoard);
+    }
+
+    public void GoHome()
+    {
+        SceneChanger.LoadScene("InSsa", objBoard);
+    }
+
 }
---
+++ SP_Client_Android/Assets/Script/SceneLoad.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour {
+
+    public Slider progressbar;
+    public Text loadText;
+
+    //public GameObject[] objText;
+
+    public string sceneName;
+
+    //스크립트 하나로밖에 줄일수있는실력밖에 안되넹..씬을 로딩으로들어와서 뿌려주는 if를 못쓰겟다 부족해서 바로할수있는실력이안됨,.일단 3개씬을만들어서 등록합니다 ㅠㅠ
+
+    private void Start()
+    {
+        //objText[0].SetActive(false);
+        //objText[1].SetActive(false);
+        StartCoroutine(LoadScene(sceneName));
+    }
+
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false; // 씬끝나면 로딩멈추는것
+
+
+        while(!operation.isDone)
+        {
+            yield return null;
+            if(progressbar.value <1f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
+                //objText[0].SetActive(true);
+            }
+            else
+            {
+                loadText.text = "로딩 완료! 기다리다 지쳤다!";
+                //objText[0].SetActive(false);
+                //objText[1].SetActive(true);
+            }
+
+            if (progressbar.value >= 1f && operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+        }
+
+    }
+
+
+}
