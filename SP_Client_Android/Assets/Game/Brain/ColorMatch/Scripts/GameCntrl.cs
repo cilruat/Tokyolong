67bc237a3d11f984bfc7b78f9a45779fc1fc11c2
cuyas <42237920+cuyas@users.noreply.github.com>
2020-12-17 05:40:03 +0900

    .
    .

--- SP_Client_Android/Assets/Game/Brain/ColorMatch/Scripts/GameCntrl.cs
+++ SP_Client_Android/Assets/Game/Brain/ColorMatch/Scripts/GameCntrl.cs
@@ -1,105 +1,120 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 namespace ColorGame
 {
-public class GameCntrl : MonoBehaviour {
+    public class GameCntrl : MonoBehaviour
+    {
 
-	float 			score;
-	GameObject [] 	colorObjects = new GameObject[4];
+        float score;
+        GameObject[] colorObjects = new GameObject[4];
 
-	public GameObject   ColorObjectPref;
-	public Vector3 []   Positions;
-	public Text 		ScoreText;
-	public Button 		RestartBtn;
-	
-	void Start () 
-	{
-		score = 0;
-		ScoreText.text = "색깔이 똑같은것을 선택해주세요!";
+        public GameObject ColorObjectPref;
+        public Vector3[] Positions;
+        public Text ScoreText;
+        public Button RestartBtn;
+        public Button RetrunHomeBtn;
 
-		InitColorObj ();	
+        void Start()
+        {
+            score = 0;
+            ScoreText.text = "색깔이 똑같은것을 선택해주세요!";
 
-		RestartBtn.GetComponent<Button>().onClick.AddListener(Restart);
+            InitColorObj();
 
-		RestartBtn.gameObject.SetActive(false);
-	}
+            RestartBtn.GetComponent<Button>().onClick.AddListener(Restart);
+            RetrunHomeBtn.GetComponent<Button>().onClick.AddListener(ReturnHome);
 
-	void InitColorObj () 
-	{
-		for (int i = 0; i < Positions.Length; i++) 
-		{
-			colorObjects [i] = Instantiate (ColorObjectPref, Positions[i], Quaternion.identity) as GameObject;
-		}
+            RestartBtn.gameObject.SetActive(false);
+            RetrunHomeBtn.gameObject.SetActive(false);
 
-		RandomizeColors ();
-	}
+        }
 
-	void RandomizeColors ()
-	{
-		Color randColor = Random.ColorHSV (0f, 1f, 
-										   0.2f, 1f, 
-										   0.2f, 1f);
+        void InitColorObj()
+        {
+            for (int i = 0; i < Positions.Length; i++)
+            {
+                colorObjects[i] = Instantiate(ColorObjectPref, Positions[i], Quaternion.identity) as GameObject;
+            }
 
-		float randRange = -.005f*score + 1f;
+            RandomizeColors();
+        }
 
-		GameObject.Find ("MainColorObject").GetComponent <ColorObject> ().SetColor (randColor, randRange);
+        void RandomizeColors()
+        {
+            Color randColor = Random.ColorHSV(0f, 1f,
+                                               0.2f, 1f,
+                                               0.2f, 1f);
 
-		SetRandomRight ();
+            float randRange = -.005f * score + 1f;
 
-		for (int i = 0; i < Positions.Length; i++) 
-		{
-			colorObjects[i].GetComponent <ColorObject> ().SetColor (randColor, randRange);
-		}		
-	}
+            GameObject.Find("MainColorObject").GetComponent<ColorObject>().SetColor(randColor, randRange);
 
-	void SetRandomRight () 
-	{
-		int right = Random.Range (0, Positions.Length); 
+            SetRandomRight();
 
-		for (int i = 0; i < Positions.Length; i++) 
-		{
-			if (i == right)
-				{ colorObjects[i].GetComponent <ColorObject> ().isRight = true; }
-			else 
-				{ colorObjects[i].GetComponent <ColorObject> ().isRight = false; }
-		}
-	}
+            for (int i = 0; i < Positions.Length; i++)
+            {
+                colorObjects[i].GetComponent<ColorObject>().SetColor(randColor, randRange);
+            }
+        }
 
-	public void GameOver ()
-	{
-		ScoreText.text = "게임오버 당신의 점수는 : " + score.ToString ();
+        void SetRandomRight()
+        {
+            int right = Random.Range(0, Positions.Length);
 
-		for (int i = 0; i < Positions.Length; i++) 
-		{
-			colorObjects[i].GetComponent <ColorObject> ().isEnable = false;
-		}
+            for (int i = 0; i < Positions.Length; i++)
+            {
+                if (i == right)
+                { colorObjects[i].GetComponent<ColorObject>().isRight = true; }
+                else
+                { colorObjects[i].GetComponent<ColorObject>().isRight = false; }
+            }
+        }
 
-		RestartBtn.gameObject.SetActive(true);
-	}
+        public void GameOver()
+        {
+            ScoreText.text = "게임오버 당신의 점수는 : " + score.ToString();
 
-	public void NextLevel () 
-	{
-		score++;
-		ScoreText.text = score.ToString ();
+            for (int i = 0; i < Positions.Length; i++)
+            {
+                colorObjects[i].GetComponent<ColorObject>().isEnable = false;
+            }
 
-		RandomizeColors ();
-	}	
+            RestartBtn.gameObject.SetActive(true);
+            RetrunHomeBtn.gameObject.SetActive(true);
+        }
 
-	void Restart ()
-	{
-		RestartBtn.gameObject.SetActive(false);
+        public void NextLevel()
+        {
+            score++;
+            ScoreText.text = score.ToString();
 
-		score = 0;
-		ScoreText.text = score.ToString ();
+            RandomizeColors();
+        }
 
-		for (int i = 0; i < Positions.Length; i++) 
-		{
-			colorObjects[i].GetComponent <ColorObject> ().isEnable = true;
-		}
+        void Restart()
+        {
+            RestartBtn.gameObject.SetActive(false);
 
-		RandomizeColors ();
-	}
-}
+            score = 0;
+            ScoreText.text = score.ToString();
+
+            for (int i = 0; i < Positions.Length; i++)
+            {
+                colorObjects[i].GetComponent<ColorObject>().isEnable = true;
+            }
+
+            RandomizeColors();
+        }
+
+
+        void ReturnHome()
+        {
+            RestartBtn.gameObject.SetActive(false);
+            SceneManager.LoadScene("ArcadeGame");
+        }
+
+    }
 }
