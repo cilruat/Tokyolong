9b22be9a0e850f407ecb9dc55d1b55c0d954d348
cuyas <cuyas@naver.com>
2021-07-16 17:12:24 +0900

    .
    .

---
+++ SP_Client_Android/Assets/UI/Order/OrderNPCMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class OrderNPCMove : MonoBehaviour {
+
+    public GameObject chrJessi;
+
+
+
+    private void Update()
+    {
+        Vector3 target = new Vector3(311, 78, 0);
+
+        chrJessi.transform.position = Vector3.MoveTowards(chrJessi.transform.position, target, 1f);
+
+    }
+
+    public void MoveToSideMenu()
+    {
+    }
+
+
+    public void MessageOnSideMenu()
+    {
+
+    }
+
+
+}
