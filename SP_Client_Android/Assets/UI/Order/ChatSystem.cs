1560cf62f94ad45cc7bcd5a54b0fdd40b90d5af4
cuyas <cuyas@naver.com>
2021-07-17 17:44:01 +0900

    .
    .

---
+++ SP_Client_Android/Assets/UI/Order/ChatSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChatSystem : MonoBehaviour {
+
+
+    public Queue<string> sentences;
+    public TextMeshPro text;
+    public string currentSentence;
+
+
+    public void Ondialogue(string[] lines)
+    {
+        sentences = new Queue<string>();
+        sentences.Clear();
+        foreach(var line in lines)
+        {
+            sentences.Enqueue(line);
+        }
+        StartCoroutine(DialogueFlow());
+    }
+
+
+    IEnumerator DialogueFlow()
+    {
+        yield return null;
+        while(sentences.Count > 0)
+        {
+            currentSentence = sentences.Dequeue();
+            text.text = currentSentence;
+            yield return new WaitForSeconds(3f);
+        }
+        Destroy(gameObject);
+    }
+
+}
---
+++ SP_Client_Android/Assets/UI/Order/NPCSentence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSentence : MonoBehaviour {
+
+
+    public string[] sentences;
+    public Transform chatTr;
+    public GameObject chatBoxPrefab;
+
+    private void Start()
+    {
+        
+    }
+
+    public void TalkNPC()
+    {
+        GameObject go = Instantiate(chatBoxPrefab);
+        go.GetComponent<ChatSystem>().Ondialogue(sentences);
+    }
+
+    private void OnMouseDown()
+    {
+        TalkNPC();
+    }
+
+
+
+}
