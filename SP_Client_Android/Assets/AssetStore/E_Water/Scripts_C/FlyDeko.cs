bd4798683514f57c1c27a45ff8a2e7c946a46bcd
cuyas <cuyas@naver.com>
2021-07-30 17:36:07 +0900

    .
    .

---
+++ SP_Client_Android/Assets/AssetStore/E_Water/Scripts_C/FlyDeko.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class FlyDeko : MonoBehaviour {
+
+    public float lookSpeed = 15.0f;
+    public float moveSpeed = 10.0f;
+
+    public float rotationX = 0.0f;
+    public float rotationY = 0.0f;
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 move_vec = Vector3.zero;
+
+        rotationX += Input.GetAxis("Mouse X") * lookSpeed;
+        rotationY += Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+        move_vec += transform.forward * Input.GetAxis("Vertical");
+        move_vec += transform.right * Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody>().AddForce(move_vec * moveSpeed);
+
+
+    }
+}
+
+
+
---
+++ SP_Client_Android/Assets/AssetStore/E_Water/Scripts_C/WaterAnim2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterAnim2 : MonoBehaviour {
+    public Texture2D[] Textures;
+    public Texture2D[] NormalTextures;
+
+    public bool NormalMapOn;
+    public int fps;
+    int counter = 0;
+
+    void Start () {
+
+        float timer = 1.0f / fps;
+        InvokeRepeating("Increment", 0, timer);
+    }
+    void Increment()
+    {
+        counter += 1;
+
+        if (counter == Textures.Length)
+        {
+            counter = 0;
+        }
+    }
+    void Update () {
+        gameObject.GetComponent<Renderer>().material.mainTexture = Textures[counter];
+        if (NormalMapOn != true) return;
+        GetComponent<Renderer>().material.SetTexture("_BumpMap", NormalTextures[counter]);
+    }
+}
