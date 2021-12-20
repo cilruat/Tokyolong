5ed4120db28455d7712b2034a34128e4b0d0d26d
cuyas <cuyas@naver.com>
2021-12-20 23:31:46 +0900

    .
    .

--- SP_Client_Android/Assets/Script/UIManager/UIGameWaiting.cs
+++ SP_Client_Android/Assets/Script/UIManager/UIGameWaiting.cs
@@ -36,7 +36,7 @@ public class UIGameWaiting : MonoBehaviour {
 
     IEnumerator _DestroyShadow()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(4f);
         UIManager.Instance.Hide(eUI.eGameWaiting);
         VersusManager.Instance.LoadPage();
     }
