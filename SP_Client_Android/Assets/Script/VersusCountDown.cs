ce57dc029e3f308954314a7998a4bd1f219fb073
cuyas <42237920+cuyas@users.noreply.github.com>
2021-12-20 20:46:31 +0900

    .
    .

---
+++ SP_Client_Android/Assets/Script/VersusCountDown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VersusCountDown : MonoBehaviour {
+
+    public int countdownTime;
+    public Text countdownDisplay;
+    public GameObject objDisplay;
+    public GameObject objBoard;
+
+    private void Start()
+    {
+        countdownDisplay.gameObject.SetActive(true);
+        StartCoroutine(CountdownToStart());
+    }
+
+
+    IEnumerator CountdownToStart()
+    {
+        while (countdownTime > 0)
+        {
+            countdownDisplay.text = countdownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+            UITweenAlpha.Start(objDisplay, 1f, 0f, TWParam.New(.5f).Curve(TWCurve.CurveLevel2));
+            UITweenScale.Start(objDisplay.gameObject, 1f, 2f, TWParam.New(.3f).Curve(TWCurve.Bounce));
+
+
+            countdownTime--;
+        }
+
+        countdownDisplay.text = "TIMEOUT!";
+
+
+
+        yield return new WaitForSeconds(1f);
+
+        SceneChanger.LoadScene("Main", objBoard);
+        Debug.Log("UI작업");
+
+        countdownDisplay.gameObject.SetActive(false);
+    }
+}
